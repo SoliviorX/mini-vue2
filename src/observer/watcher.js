efc4b22f1a7a8864f111e5966fa19ac1d95e7092
shideshan <307042873@qq.com>
2022-01-02 00:37:38 +0800

    侦听属性，用户watcher

--- src/observer/watcher.js
+++ src/observer/watcher.js
@@ -1,5 +1,6 @@
 import { pushTarget, popTarget } from "./dep";
 import { queueWatcher } from "./scheduler";
+import { isObject } from "../util/index";
 
 // 全局变量id  每次new Watcher都会自增
 let id = 0;
@@ -9,29 +10,47 @@ export default class Watcher {
     this.exprOrFn = exprOrFn;
     this.cb = cb;
     this.options = options;
+    this.user = !!options.user; // 表示是不是用户watcher
+
     this.id = id++; // watcher的唯一标识
 
     this.deps = []; //存放dep的容器
     this.depsId = new Set(); //用来去重dep
 
-    this.getter = exprOrFn;
-    this.get();
+    /**
+     * 1. 渲染watcher中，exprOrFn为updateComponent()，是一个函数
+     * 2. 在用户watcher中，exprOrFn为字符串（watch中的属性名，即监听地属性）
+     */
+    // 当是渲染watcher时
+    if (typeof exprOrFn === "function") {
+      this.getter = exprOrFn;
+    } else {
+      // 当是用户自定义watcher时
+      this.getter = function () {
+        // watch的key可能是 'obj1.a.d' 这种格式，需要处理成vm.obj1.a.d
+        let path = exprOrFn.split(".");
+        let obj = vm;
+        for (let i = 0; i < path.length; i++) {
+          obj = obj[path[i]]; //vm.a.a.a.a.b
+        }
+        return obj; // 执行getter()，返回obj，会触发依赖收集，将用户watcher收集到监听的数据上
+      };
+    }
+
+    this.value = this.get();
   }
 
   // new Watcher时会执行get方法；之后数据更新时，直接手动调用get方法即可
   get() {
     // 把当前watcher放到全局栈，并设置Dep.target（无法继承，具唯一性）为当前watcher
     pushTarget(this);
-
     /**
      * 执行exprOrFn，如果watcher是渲染watcher，则exprOrFn为vm._update(vm._render())
      * 在执行render函数的时候，获取变量会触发属性的getter（定义在对象数据劫持中），在getter中进行依赖收集
      */
-    const res = this.getter.call(this.vm);
-
+    const res = this.getter.call(this.vm); // 如果是用户watcher，则上一次执行getter得到的值即为oldValue
     // 执行完getter就把当前watcher删掉，以防止用户在methods/生命周期中访问data属性时进行依赖收集（数据劫持时会判断Dep.target是否存在）
     popTarget(); // 在调用方法之后把当前watcher实例从全局watcher栈中移除，设置Dep.target为新的栈顶watcher
-
     return res;
   }
 
@@ -61,7 +80,19 @@ export default class Watcher {
   }
 
   run() {
-    // TODO 其他功能扩展
-    this.getter.call(this.vm)
+    // 执行getter，更新视图/获取新值
+    const newVal = this.getter(); // 新值
+    const oldVal = this.value; //老值
+    this.value = newVal; // newVal就成为了现在的值；为了保证下一次更新时，上一次的新值是下一次的老值
+
+    // 如果是用户watcher
+    if (this.user) {
+      if (newVal !== oldVal || isObject(newVal)) {
+        this.cb.call(this.vm, newVal, oldVal);
+      }
+    } else {
+      // 渲染watcher
+      this.cb.call(this.vm);
+    }
   }
 }
--- src/state.js
+++ src/state.js
@@ -4,6 +4,7 @@
 
 import { observe } from "./observer/index";
 import { isFunction } from "./utils";
+import Watcher from "./observer/watcher";
 
 export function initState(vm) {
   const opts = vm.$options;
@@ -13,44 +14,82 @@ export function initState(vm) {
   if (opts.methods) {
     initMethod(vm);
   }
+  // 初始化data
   if (opts.data) {
-    // 初始化data
     initData(vm);
   }
   if (opts.computed) {
     initComputed(vm);
   }
+  // 初始化watch
   if (opts.watch) {
     initWatch(vm);
   }
+}
 
-  function initProps() {}
-  function initMethod() {}
-  function initData(vm) {
-    let data = vm.$options.data;
-    // 往实例上添加一个属性 _data，即传入的data
-    // vue组件data推荐使用函数 防止数据在组件之间共享
-    data = vm._data = isFunction(data) ? data.call(vm) : data;
+function initProps() {}
+function initMethod() {}
+function initData(vm) {
+  let data = vm.$options.data;
+  // 往实例上添加一个属性 _data，即传入的data
+  // vue组件data推荐使用函数 防止数据在组件之间共享
+  data = vm._data = isFunction(data) ? data.call(vm) : data;
 
-    // 将vm._data上的所有属性代理到 vm 上
-    for (let key in data) {
-      proxy(vm, "_data", key);
+  // 将vm._data上的所有属性代理到 vm 上
+  for (let key in data) {
+    proxy(vm, "_data", key);
+  }
+  // 对数据进行观测 -- 数据响应式
+  observe(data);
+}
+function initComputed() {}
+
+// 初始化watch
+function initWatch(vm) {
+  let watch = vm.$options.watch;
+  for (let k in watch) {
+    const handler = watch[k]; // 可能是数组、对象、函数、字符串，watch支持多种写法
+    if (Array.isArray(handler)) {
+      handler.forEach((handle) => {
+        createWatcher(vm, k, handle);
+      });
+    } else {
+      createWatcher(vm, k, handler);
     }
-    // 对数据进行观测 -- 数据响应式
-    observe(data);
   }
-  function initComputed() {}
-  function initWatch() {}
+}
 
-  // 将vm._data上的属性代理到 vm 上
-  function proxy(vm, source, key) {
-    Object.defineProperty(vm, key, {
-      get() {
-        return vm[source][key];
-      },
-      set(newValue) {
-        vm[source][key] = newValue;
-      },
-    });
+function createWatcher(vm, key, handler, options = {}) {
+  if (typeof handler === "object") {
+    options = handler; //保存用户传入的对象
+    handler = handler.handler; //是函数
   }
+  if (typeof handler === "string") {
+    handler = vm[handler];
+  }
+  // watch相当于调用了 vm.$watch()
+  return vm.$watch(key, handler, options);
+}
+
+// 将vm._data上的属性代理到 vm 上
+function proxy(vm, source, key) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[source][key];
+    },
+    set(newValue) {
+      vm[source][key] = newValue;
+    },
+  });
+}
+
+export function stateMixin(Vue) {
+  Vue.prototype.$watch = function (exprOrFn, cb, options) {
+    const vm = this;
+    // 这里表示是一个用户watcher
+    let watcher = new Watcher(vm, exprOrFn, cb, { ...options, user: true });  // user: true表示该watcher为用户自己创建地watcher
+    if (options.immediate) {
+      cb(watcher.value); // 如果立刻执行
+    }
+  };
 }
